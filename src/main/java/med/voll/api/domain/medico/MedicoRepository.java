2775fac2ec8b98c816e35ba8d3e46d484eb21a16
“Calixto” <l19070031@huetamo.tecnm.mx>
2024-06-28 15:57:13 -0600

    Agregando archivos al repositorio

---
+++ src/main/java/med/voll/api/domain/medico/MedicoRepository.java
@@ -0,0 +1,37 @@
+package med.voll.api.domain.medico;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import java.time.LocalDateTime;
+
+@Repository
+public interface MedicoRepository extends JpaRepository<Medico, Long> {
+    Page<Medico> findByActivoTrue(Pageable paginacion);
+
+    @Query(value = """
+            select m.* from Medico m
+            where m.activo = 1
+            and m.especialidad = :especialidad 
+            and m.id not in(  
+                select c.medico_id from Consulta c
+                where
+                c.fecha = :fecha
+            )
+            order by rand()
+            limit 1
+            """, nativeQuery = true)
+    Medico seleccionarMedicoConEspecialidadEnFecha(@Param("especialidad") Especialidad especialidad, @Param("fecha") LocalDateTime fecha);
+
+
+    @Query(value = """
+            select m.activo 
+            from Medico m
+            where m.id=:idMedico
+            """)
+    Boolean findActivoById(Long idMedico);
+}
